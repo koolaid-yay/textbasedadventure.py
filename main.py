2019d66d536151c770eb433e516ea4010764487b
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-25 19:43:23 +0000

    Co-authored-by: No-One117 <No-One117@users.noreply.github.com> Co-authored-by: mnerelong <mnerelong@users.noreply.github.com> Co-authored-by: pythonprogram <ianlowryucas@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
+#LL AS IL MM Python Final
+
+#Meika Milton v
+inventory = [0]
+story_point = 1
+#Ian v
+rooms = ["outside", ]
+health = 4
+
+
+name = input("before you start your journey, please enter your name here: ") #adelheididthhis
+print("commands n = north w = west e = east s = south i = inventory y = yes n = no") #ian wuz here
+print("In a dry desert lays a massive pyramid. Encompassing you in its shadow. Making a cold resting place for weary travelers. \n")
+print("a torch lays infront of you  half burried in the sand")
+room = "desert"
+
+
+pick_up = input("do you pick it up : ")
+
+    #room2 desc
+#print('you find your self in arather unsuspecting looking room. there is sword in the center of the room')
+#input('do you pick up the sword')
+#if y then health -= 4r
+
+
+#lucas is silly\/
+
+if pick_up == "y":
+    inventory.append("torch")
+    print("a door opens in front of you do you enter:")
+else:
+    print("nah im too tired i'll come back tomorrow.") 
+
+
+
+
+
 
