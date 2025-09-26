8eefca11a506b42916e6d72619c12aadc991399b
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-26 13:20:07 -0600

    Co-authored-by: mnerelong <mnerelong@users.noreply.github.com> Co-authored-by: pythonprogram <ianlowryucas@users.noreply.github.com> Co-authored-by: No-One117 <No-One117@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,35 +4,31 @@
 inventory = [0]
 story_point = 1
 #Ian v
-rooms = ["outside", ]
+rooms = ["outside", 'trap_room']
 health = 4
 
 
-name = input("before you start your journey, please enter your name here: ") #adelheididthhis
+name = input("before you start your journey, please enter your name here : ") #adelheiddidthis
 print("commands n = north w = west e = east s = south i = inventory y = yes n = no") #ian wuz here
 print("In a dry desert lays a massive pyramid. Encompassing you in its shadow. Making a cold resting place for weary travelers. \n")
 print("a torch lays infront of you  half burried in the sand")
-room = "desert"
+rooms[0]
 
-
-pick_up = input("do you pick it up : ")
-
-    #room2 desc
-#print('you find your self in arather unsuspecting looking room. there is sword in the center of the room')
-#input('do you pick up the sword')
-#if y then health -= 4r
-
+check = input("do you pick it up : ")
 
 #lucas is silly\/
 
-if pick_up == "y":
+if check == "y":
     inventory.append("torch")
-    print("a door opens in front of you do you enter:")
+    check=input("a door opens in front of you do you enter : ")
 else:
-    print("nah im too tired i'll come back tomorrow.") 
+    print("nah im too tired i'll come back tomorrow.")
+#ian wuz here
+if check == "y":
+    print('you find your self in a rather unsuspecting looking room. there is sword in the center of the room')
+    check=input('do you pick up the sword : ')
+if check=='y':
+    health-=4
 
-
-
-
-
-
+if check=='y':
+   check == 4
